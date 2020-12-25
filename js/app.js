e2d6ad2f765298b48e57c718a8915f640e598759
JFelixZuniga <johnfzuniga@gmail.com>
2020-12-25 17:06:28 -0300

    3er commit, finalizando js junto a filtrado

--- js/app.js
+++ js/app.js
@@ -1,21 +1,79 @@
 // VARIABLES
+const marca = document.querySelector("#marca");
 const year = document.querySelector("#year");
+const minimo = document.querySelector("#minimo");
+const maximo = document.querySelector("#maximo");
+const puertas = document.querySelector("#puertas");
+const transmision = document.querySelector("#transmision");
+const color = document.querySelector("#color");
+
+// Contenedor para los resultados
 const resultado = document.querySelector("#resultado");
 
 const max = new Date().getFullYear();
 const min = max - 10;
 
+// Generamos un objeto con la búsqueda
+const datosBusqueda = {
+  marca: "",
+  year: "",
+  minimo: "",
+  maximo: "",
+  puertas: "",
+  transmision: "",
+  color: "",
+};
+
 // EVENTOS
 document.addEventListener("DOMContentLoaded", () => {
   // Muestra ls vehículos al cargar
-  mostrarAutos();
+  mostrarAutos(autos);
 
   // Llena las opciones de años
   llenarSelect();
 });
 
+// Event listnener para los select de búsqueda
+marca.addEventListener("change", (e) => {
+  datosBusqueda.marca = e.target.value;
+
+  filtrarAuto();
+});
+year.addEventListener("change", (e) => {
+  datosBusqueda.year = parseInt(e.target.value);
+
+  filtrarAuto();
+});
+minimo.addEventListener("change", (e) => {
+  datosBusqueda.minimo = e.target.value;
+
+  filtrarAuto();
+});
+maximo.addEventListener("change", (e) => {
+  datosBusqueda.maximo = e.target.value;
+
+  filtrarAuto();
+});
+puertas.addEventListener("change", (e) => {
+  datosBusqueda.puertas = parseInt(e.target.value);
+
+  filtrarAuto();
+});
+transmision.addEventListener("change", (e) => {
+  datosBusqueda.transmision = e.target.value;
+
+  filtrarAuto();
+});
+color.addEventListener("change", (e) => {
+  datosBusqueda.color = e.target.value;
+
+  filtrarAuto();
+});
+
 // FUNCIONES
-function mostrarAutos() {
+function mostrarAutos(autos) {
+  limpiarHTML();
+
   autos.forEach((auto) => {
     const autoHTML = document.createElement("p");
 
@@ -29,13 +87,97 @@ function mostrarAutos() {
     resultado.appendChild(autoHTML);
   });
 }
+
+// Limpiar Html
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
 // Genera los años del select
 function llenarSelect() {
   for (let i = max; i >= min; i--) {
     const opcion = document.createElement("option");
     opcion.value = i;
     opcion.textContent = i;
-    // Agregamos las opciones de año al select
-    year.appendChild(opcion)
+    // Agregamos las opciones de año al selectclear
+    year.appendChild(opcion);
   }
 }
+
+// Función que filtra en base a la búsqueda
+function filtrarAuto() {
+  const resultado = autos
+    .filter(filtrarMarca)
+    .filter(filtrarYear)
+    .filter(filtrarMinimo)
+    .filter(filtrarMaximo)
+    .filter(filtrarPuertas)
+    .filter(filtrarTransmision)
+    .filter(filtrarColor);
+
+  console.log(resultado);
+
+  mostrarAutos(resultado);
+}
+
+function filtrarMarca(auto) {
+  const { marca } = datosBusqueda;
+  if (marca) {
+    return auto.marca === datosBusqueda.marca;
+  }
+  return auto;
+}
+
+function filtrarYear(auto) {
+  const { year } = datosBusqueda;
+  if (year) {
+    return auto.year === year;
+  }
+  return auto;
+}
+
+function filtrarMinimo(auto) {
+  const { minimo } = datosBusqueda;
+  if (minimo) {
+    return auto.precio >= minimo;
+  }
+  return auto;
+}
+
+function filtrarMaximo(auto) {
+  const { maximo } = datosBusqueda;
+  if (maximo) {
+    return auto.precio <= maximo;
+  }
+  return auto;
+}
+
+function filtrarPuertas(auto) {
+  const { puertas } = datosBusqueda;
+  if (puertas) {
+    return auto.puertas === puertas;
+  }
+  return auto;
+}
+
+function filtrarTransmision(auto) {
+  const { transmision } = datosBusqueda;
+  if (transmision) {
+    return auto.transmision === transmision;
+  }
+  return auto;
+}
+
+function filtrarColor(auto) {
+  const { color } = datosBusqueda;
+  if (color) {
+    return auto.color === color;
+  }
+  return auto;
+}
+
+/*
+  En principo estamos ocupando el Ppio. de Programación Fucional, donde el código se basa en escribir funciones que en primera cuenta no modifican el arreglo original, y todo está dividido en pequeñas funciones que realizan ciertas operaciones.
+*/
